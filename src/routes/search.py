0d82b0f85e3a449875c87b5bf22ae7eb6a2fd88d
sedwards8900 <sedwards70@student.gsu.edu>
2021-11-05 00:45:06 -0400

    Added search and profile routes as well as html pages

---
+++ src/routes/search.py
@@ -0,0 +1,27 @@
+from os import stat
+from flask import Blueprint, render_template
+import routes.util as util
+
+search_blueprint = Blueprint(
+    "bp_search",
+    __name__,
+    template_folder=util.get_templates_folder(),
+    static_folder=util.get_static_folder(),
+)
+
+
+def get_blueprint():
+    """
+    Returns the `Blueprint` object which stores all of the functions in this file.
+
+    All routing files have the same function to retrieve their blueprints.
+
+    Returns:
+        The blueprint object for this file.
+    """
+    return search_blueprint
+
+
+@search_blueprint.route("/search")
+def login():
+    return render_template("search.html")
