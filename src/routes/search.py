371c2305cf3173ffa8f570c6d83b1abd0e750809
suzannealexander <suzanne@upperlevelcrm.com>
2021-11-10 12:38:11 -0500

    searchRecipes
    adding basic functionality for searching for recipes by ingredients

--- src/routes/search.py
+++ src/routes/search.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+import flask
+from flask import Blueprint, render_template, request
 import routes.util as util
+import api.spoonacular
+
+# import spoonacular somehow
 
 search_blueprint = Blueprint(
     "bp_search",
@@ -19,6 +23,25 @@ def get_blueprint():
         The blueprint object for this file.
     """
     return search_blueprint
+
+
+@search_blueprint.route("/search_ingredients")
+def search_ingredients():
+    return render_template("search.html")
+
+
+@search_blueprint.route("/search_recipes", methods=["POST"])
+def search_recipes():
+    if flask.request.method == "POST":
+        ingredients = flask.request.form.get("searchRecipes")
+        # prints what the user searches for to the terminal
+        #
+        print(ingredients)
+        returnedDict = api.spoonacular.search_recipes(ingredients)
+        # prints the 10 recipes to the terminal
+        print(returnedDict)
+        return render_template("index.html", recipes=returnedDict)
+    return render_template("index.html")
 
 
 @search_blueprint.route("/search")
