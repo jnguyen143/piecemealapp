2e0fb09749301b3a2bfa950847b3da451d9712ce
William Garland <garland.william99@gmail.com>
2021-11-21 00:41:39 -0500

    Fixed remaining linter errors

--- src/app/gen_keys.py
+++ src/app/gen_keys.py
@@ -1,3 +1,7 @@
+"""
+Generates public and private keypairs.
+"""
+
 from Crypto.PublicKey import RSA
 
 
@@ -6,7 +10,7 @@ def generate_keys():
     Function that writes and exports the application's private and public keys
     """
     keypair = RSA.generate(2048)
-    with open("../private_key.pem", "wb") as f:
-        f.write(keypair.exportKey())
-    with open("../public_key.pem", "wb") as f:
-        f.write(keypair.publickey().exportKey())
+    with open("../private_key.pem", "wb") as file:
+        file.write(keypair.exportKey())
+    with open("../public_key.pem", "wb") as file:
+        file.write(keypair.publickey().exportKey())
