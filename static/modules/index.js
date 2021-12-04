a30f710f88d5de337a9cc91faacd944ed068e0fd
sedwards8900 <sedwards70@student.gsu.edu>
2021-12-03 22:23:02 -0500

    Set up Account settings page html

--- static/modules/index.js
+++ static/modules/index.js
@@ -1,27 +1,14 @@
 import { showToast } from './Toast.js';
 
-function notAbleToAddToast() {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const btn of document.getElementsByClassName('add-button')) {
-    btn.addEventListener('click', (event) => {
-      showToast("You need to be logged in to save recipes and ingredients", 6000);
-      // const id = event.target.getAttribute('ingredient-id');
-      // const name = event.target.getAttribute('ingredient-name');
-      // const image = event.target.getAttribute('ingredient-image');
-      // addIngredient(id, name, image, true);
-    });
-  }
 
-  document.getElementById('secretbutton2').addEventListener('click', (secretevent) => {
-    notAbleToAddToast();
-
-  // for (let btn of document.getElementsByClassName("add-button")) {
-  //   btn.addEventListener("click", () => {
-  //     showToast("You need to be logged in to save recipes and ingredients", 6000);
-  //   });
-  // }
-
-
+// eslint-disable-next-line no-restricted-syntax, no-unused-vars
+// document.getElementById('secretbutton').addEventListener('click', (secretevent) => {
+// eslint-disable-next-line no-restricted-syntax
+for (const btn of document.getElementsByClassName('add-recipe-button')) {
+  btn.addEventListener('click', (event) => {
+    showToast("You need to be logged in to save recipes and ingredients", 6000);
+  });
+}
 
 
 // function searchRecipes() {
