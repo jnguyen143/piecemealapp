3f810d090a11dcd777bbcb54917f5fd446551f0e
sedwards8900 <sedwards70@student.gsu.edu>
2021-12-03 21:24:30 -0500

    Fixed add function error on index.html page

--- static/modules/index.js
+++ static/modules/index.js
@@ -1,10 +1,25 @@
 import { showToast } from './Toast.js';
 
-for (let btn of document.getElementsByClassName("add-button")) {
-  btn.addEventListener("click", () => {
-    showToast("You need to be logged in to save recipes and ingredients", 6000);
-  });
-}
+function notAbleToAddToast() {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const btn of document.getElementsByClassName('add-button')) {
+    btn.addEventListener('click', (event) => {
+      showToast("You need to be logged in to save recipes and ingredients", 6000);
+      // const id = event.target.getAttribute('ingredient-id');
+      // const name = event.target.getAttribute('ingredient-name');
+      // const image = event.target.getAttribute('ingredient-image');
+      // addIngredient(id, name, image, true);
+    });
+  }
+
+  document.getElementById('secretbutton2').addEventListener('click', (secretevent) => {
+    notAbleToAddToast();
+
+  // for (let btn of document.getElementsByClassName("add-button")) {
+  //   btn.addEventListener("click", () => {
+  //     showToast("You need to be logged in to save recipes and ingredients", 6000);
+  //   });
+  // }
 
 
 
--- static/modules/login.js
+++ static/modules/login.js
@@ -11,12 +11,6 @@ async function startLogin(data) {
   });
 }
 
-// Function to hide the Spinner
-function hideSpinner() {
-  document.getElementById('spinner')
-    .style.display = 'none';
-}
-
 document.getElementById('default-login-form').onsubmit = () => {
   // Encrypt the data, then send it to the server
   encryptData(JSON.stringify({
@@ -26,7 +20,6 @@ document.getElementById('default-login-form').onsubmit = () => {
   })).then((data) => {
     startLogin(data).then((response) => response.json()).then((response) => {
       if (response.success) {
-        hideSpinner();
         window.location.href = '/';
       } else {
         showToast('Failed to log in - Invalid credentials');
@@ -42,7 +35,9 @@ document.getElementById('default-login-form').onsubmit = () => {
 document.getElementById('google-login-form').onsubmit = () => {
   encryptData(JSON.stringify({ authentication: 1 })).then((data) => {
     startLogin(data).then((response) => response.json()).then((response) => {
-      if (response.success) { window.location.replace(response.redirect_url); } else {
+      if (response.success) {
+        window.location.replace(response.redirect_url);
+      } else {
         showToast('Failed to log in - Invalid credentials');
       }
     });
